54fa6be3478dfea164f05a77159c50aff9d6ee09
Erez <erezfree29@gmail.com>
2021-04-27 14:17:09 -0700

    fix rubocop linters errors

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -33,6 +33,6 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:name, :date, :author_id, :description , :location)
+    params.require(:event).permit(:name, :date, :author_id, :description, :location)
   end
 end
