763729ba1c6bd263c7d0a0f2937549be29942142
Erez <erezfree29@gmail.com>
2021-04-28 13:30:05 -0700

    add association to events trough event_attendees

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :author, class_name: 'User'
+  has_many :attendees, through: :event_attendees, class_name: 'User', foreign_key: :user_id
   validates :name, :date, :location, :author_id, presence: true
   scope :upcoming, -> { where('date >= ?', Time.zone.now) }
   scope :previous, -> { where('date < ?', Time.zone.now) }
